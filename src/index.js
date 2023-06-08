a2e73a87efff99f82a6820584cd7b732bd7c8247
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-06-08 11:22:47 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './JS/stars-rating';
 import './JS/header.js';
 import './JS/hero.js';
 import './JS/weekly-trends-main';
-
 import './upcoming/upcoming';
 import './JS/modal-trailer';
 import './JS/global-listener';
