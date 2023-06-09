0f5660d1fa1a892b1ac3fa9768650c085d5c7229
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-06-09 19:34:42 +0300

    Update global-listener.js

--- src/javascript/global-listener.js
+++ src/javascript/global-listener.js
@@ -1,14 +1,10 @@
 import { openModalAboutFilm } from './movieModal';
 import { handleFilm } from './library/library';
-// import { playTrailer } from './modal-trailer';
 
-window.addEventListener('click', (e) => {
+window.addEventListener('click', e => {
   const heroDetailsButton = document.querySelector('.css-bnt-info');
   const watchTrailerButton = document.querySelector('.watch-trailer-button');
-  // const addBtn = document.querySelector('[data-add]');
-  // const removeBtn = document.querySelector('[data-remove]');
   const upcomingBtn = document.querySelector('.btn');
-
 
   const id = e.target.dataset.id;
 
@@ -17,19 +13,11 @@ window.addEventListener('click', (e) => {
       openModalAboutFilm(id);
       break;
 
-    // case addBtn:
-    //   handleFilm(e)
-    //   break;
-
     case upcomingBtn:
-    // case removeBtn:
-      handleFilm(e)
+      handleFilm(e);
       break;
 
     case watchTrailerButton:
-      // const movieId = watchTrailerButton.getAttribute('trailer-id');
-      // playTrailer(movieId);
-      // trailerCb();
       break;
 
     default:
