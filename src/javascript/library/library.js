91e9d7976926168abc14c751f682087a5a7e261f
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-06-09 08:40:04 +0300

    update

--- src/javascript/library/library.js
+++ src/javascript/library/library.js
@@ -6,110 +6,110 @@ import { errorMarkup, onCreateLibraryMarkup } from './library-markup';
 const libraryRef = document.querySelector('.library-card-list');
 const btnLib = document.querySelector('.btn');
 
-window.addEventListener('DOMContentLoaded', () => {
-  const library = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-  // if(libraryRef) createLibraryMarkup(library);
-  let totalElementInList = 0;
-  let firstEl = 0;
-  getLibrarylistInParts(library);
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//   const library = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+//   // if(libraryRef) createLibraryMarkup(library);
+//   let totalElementInList = 0;
+//   let firstEl = 0;
+//   getLibrarylistInParts(library);
+// });
 
-btnLib?.addEventListener('click', function (event) {
-  getLibrarylistInParts(library);
-  firstEl += 6;
-  totalElementInList += 6;
-});
+// btnLib?.addEventListener('click', function (event) {
+//   getLibrarylistInParts(library);
+//   firstEl += 6;
+//   totalElementInList += 6;
+// });
 
-let totalElementInList = 0;
-let firstEl = 0;
-let lostEl = firstEl + 6;
+// let totalElementInList = 0;
+// let firstEl = 0;
+// let lostEl = firstEl + 6;
 
-function getLibrarylistInParts(library) {
-  const totalLiberyLength = library.length;
-  const libraryInParts = library.slice(firstEl, lostEl);
-  if (totalLiberyLength - totalElementInList <= 6) {
-    onCreateLibraryMarkup(libraryInParts);
-  } else {
-    onCreateLibraryMarkup(libraryInParts);
-    libraryRef.insertAdjacentHTML(
-      'afterend',
-      ` <div class="lib-btn-cont"><button class="btn btn-search-movie" type="button">Load more</button></div>`
-    );
-  }
-}
+// function getLibrarylistInParts(library) {
+//   const totalLiberyLength = library.length;
+//   const libraryInParts = library.slice(firstEl, lostEl);
+//   if (totalLiberyLength - totalElementInList <= 6) {
+//     createLibraryMarkup(libraryInParts);
+//   } else {
+//     createLibraryMarkup(libraryInParts);
+//     libraryRef.insertAdjacentHTML(
+//       'afterend',
+//       ` <div class="lib-btn-cont"><button class="btn btn-search-movie" type="button">Load more</button></div>`
+//     );
+//   }
+// }
 
-export function handleFilm(e) {
-  const id = e.target.dataset.id;
+// export function handleFilm(e) {
+//   const id = e.target.dataset.id;
 
-  if (e.target.hasAttribute('data-add')) {
-    // btnLib.removeAttribute('data-add');
-    // btnLib.setAttribute('data-remove', '');
-    // btnLib.textContent = 'Remove from my library';
-    setBtnProp(e.target, addOps);
+//   if (e.target.hasAttribute('data-add')) {
+//     // btnLib.removeAttribute('data-add');
+//     // btnLib.setAttribute('data-remove', '');
+//     // btnLib.textContent = 'Remove from my library';
+//     setBtnProp(e.target, addOps);
 
-    addFilmToLibrary(id);
+//     addFilmToLibrary(id);
 
-    //
-  } else if (e.target.hasAttribute('data-remove')) {
-    console.log('data-remove');
+//     //
+//   } else if (e.target.hasAttribute('data-remove')) {
+//     console.log('data-remove');
 
-    e.target.removeAttribute('data-remove');
-    e.target.setAttribute('data-add', '');
-    e.target.textContent = 'Add to my library';
+//     e.target.removeAttribute('data-remove');
+//     e.target.setAttribute('data-add', '');
+//     e.target.textContent = 'Add to my library';
 
-    deleteCardLibrary(id);
-  }
-}
+//     deleteCardLibrary(id);
+//   }
+// }
 
-const addOps = {
-  addAttr: 'data-remove',
-  removeAttr: 'data-add',
-  btnText: 'Remove from my library',
-};
+// const addOps = {
+//   addAttr: 'data-remove',
+//   removeAttr: 'data-add',
+//   btnText: 'Remove from my library',
+// };
 
-function setBtnProp(el, props) {
-  const { addAttr, removeAttr, btnText } = props;
+// function setBtnProp(el, props) {
+//   const { addAttr, removeAttr, btnText } = props;
 
-  el.removeAttribute(removeAttr);
-  el.setAttribute(addAttr, '');
-  el.textContent = btnText;
-}
+//   el.removeAttribute(removeAttr);
+//   el.setAttribute(addAttr, '');
+//   el.textContent = btnText;
+// }
 
-/////// ПРОВЕРКА НАЛИЧИЯ ID В LOCAL STORAGE /////////
+// /////// ПРОВЕРКА НАЛИЧИЯ ID В LOCAL STORAGE /////////
 
-/////// ПОЛУЧЕНИЯ ОТ СЕРВЕРА ФИЛЬМА ПО ID ///////
+// /////// ПОЛУЧЕНИЯ ОТ СЕРВЕРА ФИЛЬМА ПО ID ///////
 
-async function getMovieById(id) {
-  const responce = await API.getMoviById(id);
-  return responce.data;
-}
+// async function getMovieById(id) {
+//   const responce = await API.getMoviById(id);
+//   return responce.data;
+// }
 
-///// ФУНКЦИЯ ДОБАВЛЕНИЯ В LOCAL STORAGE ///////
+// ///// ФУНКЦИЯ ДОБАВЛЕНИЯ В LOCAL STORAGE ///////
 
-export async function addFilmToLibrary(id) {
-  const libraryList = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-  const isAtLibrary = libraryList?.find(x => x.id === Number(id));
+// export async function addFilmToLibrary(id) {
+//   const libraryList = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+//   const isAtLibrary = libraryList?.find(x => x.id === Number(id));
 
-  if (isAtLibrary) return console.log('film is at list');
+//   if (isAtLibrary) return console.log('film is at list');
 
-  const movieObj = await getMovieById(id);
-  libraryList.push(movieObj);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(libraryList));
-}
+//   const movieObj = await getMovieById(id);
+//   libraryList.push(movieObj);
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(libraryList));
+// }
 
-///// ФНКЦИЯ УДАЛЕНИЯ ИЗ LOCAL STORAGE ///////
+// ///// ФНКЦИЯ УДАЛЕНИЯ ИЗ LOCAL STORAGE ///////
 
-export function deleteCardLibrary(id) {
-  const libraryList = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  const itemToDelete = libraryList.find(film => film.id === id);
+// export function deleteCardLibrary(id) {
+//   const libraryList = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   const itemToDelete = libraryList.find(film => film.id === id);
 
-  libraryList.pop(itemToDelete);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(libraryList));
+//   libraryList.pop(itemToDelete);
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(libraryList));
 
-  if (location.pathname === '/library.html') {
-    // libraryRef.innerHTML = '';
-    createLibraryMarkup(libraryList);
-  }}
+//   if (location.pathname === '/library.html') {
+//     // libraryRef.innerHTML = '';
+//     createLibraryMarkup(libraryList);
+//   }}
 
 function createLibraryMarkup(libraryInParts) {
   if (libraryInParts.length === 0) {
